aaf1c4ea084551ca4ff89f01dad787add6ffc28c
Abolfazl Panahiazar <abolfazlpanahiazar@gmail.com>
2021-06-28 09:45:36 +0430

    Updates

---
+++ precache-manifest.14b84d6d0ce017bb35b7c09de9cf6c2d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d159386b0ca222430c1c3aef7ccd2cf5",
+    "url": "/Reactstrap-Crud/index.html"
+  },
+  {
+    "revision": "fc5f10f432280fee0eb6",
+    "url": "/Reactstrap-Crud/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9e5e2c5ff23b683f164e",
+    "url": "/Reactstrap-Crud/static/css/main.700e8687.chunk.css"
+  },
+  {
+    "revision": "fc5f10f432280fee0eb6",
+    "url": "/Reactstrap-Crud/static/js/2.b2da5290.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Reactstrap-Crud/static/js/2.b2da5290.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e5e2c5ff23b683f164e",
+    "url": "/Reactstrap-Crud/static/js/main.5a0aa141.chunk.js"
+  },
+  {
+    "revision": "58dae6b681910c230b0c",
+    "url": "/Reactstrap-Crud/static/js/runtime-main.b76b9f2b.js"
+  }
+]);
